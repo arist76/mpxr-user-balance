3911f5194c284541863eb13b1f18f1edac806e47
Surafel <surafelfikru76@gmail.com>
2024-12-31 10:12:41 +0300

    Initial commit

---
+++ main.py
@@ -0,0 +1,11 @@
+""" 
+A Standalone module that generates a list of all users and their balances on
+the network and exports it in json.
+"""
+
+def main():
+    pass
+
+
+if __name__ == '__main__':
+    main()
